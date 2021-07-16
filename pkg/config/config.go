8e97dfad564e297eaad9aca834398fc9c5b90415
Lucas Desouza <lucasfds2777@gmail.com>
2021-07-16 00:02:15 -0500

    troubleshooting secret setup

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -7,6 +7,8 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"os"
+	"strings"
 )
 
 var config *viper.Viper
@@ -19,11 +21,17 @@ func Init(env string) {
 	config.SetConfigType("yaml")
 	config.AddConfigPath(".")
 	config.SetEnvPrefix("secret")
-	config.BindEnv("datasource.password")
 
 	err := config.ReadInConfig()
 	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+	}
+
+	for _, k := range config.AllKeys() {
+		value := config.GetString(k)
+		if strings.HasPrefix(value, "${") && strings.HasSuffix(value, "}") {
+			config.Set(k, getEnvOrPanic(strings.TrimSuffix(strings.TrimPrefix(value,"${"), "}")))
+		}
 	}
 
 	host := config.GetString("datasource.host")
@@ -33,6 +41,7 @@ func Init(env string) {
 	port := config.GetString("datasource.port")
 	sslmode := config.GetString("datasource.sslmode")
 	timeZone := config.GetString("datasource.timeZone")
+
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s", host, user, password, dbname, port, sslmode, timeZone)
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -47,4 +56,12 @@ func GetConfig() *viper.Viper {
 
 func GetDataSource() *gorm.DB {
 	return db
+}
+
+func getEnvOrPanic(env string) string {
+	res := os.Getenv(env)
+	if len(res) == 0 {
+		panic("Mandatory env variable not found:" + env)
+	}
+	return res
 }
